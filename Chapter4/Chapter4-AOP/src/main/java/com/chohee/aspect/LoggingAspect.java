5e846d7c37ecf090590e5e4dda4b1aeafe6051b5
Chohee Kim <kch5559@gmail.com>
2016-09-06 14:04:00 -0700

    Different type of pointcuts

--- Chapter4/Chapter4-AOP/src/main/java/com/chohee/aspect/LoggingAspect.java
+++ Chapter4/Chapter4-AOP/src/main/java/com/chohee/aspect/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.chohee.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Created by Chohee on 8/29/16.
@@ -11,6 +12,7 @@ import org.aspectj.lang.annotation.Before;
 @Aspect
 public class LoggingAspect {
 
+    /**
     //specifying target for particular class/object
     @Before("execution(public String com.chohee.model.Circle.getName())")
     public void loggingAdviceForCircle() {
@@ -22,4 +24,31 @@ public class LoggingAspect {
     public void loggingAdviceForTriangle() {
         System.out.println("Advice runs. Get Method called for Triangle class");
     }
+    **/
+
+    @Before("allGetters()")
+    public void LoggingAdvice() {
+        System.out.println("Advice runs. * Get* Method called.");
+    }
+
+    @Before("allCircleMethods()")
+    public void LoggingAdviceForCircle() {
+        System.out.println("Advice for all circle methods. One of the methods in circle get called");
+    }
+
+    @Before("allTriangleMethods()")
+    public void  LoggingAdviceForTriangle() {
+        System.out.println("Advice for all triangle methods. One of the methods in triangle get called");
+    }
+
+
+    @Pointcut("execution(* get*())")
+    public void allGetters() {} //dummy method, which holds a pointcut expression
+
+    @Pointcut("execution(* com.chohee.model.Circle.*(..))")
+    public void allCircleMethods() {}
+
+    @Pointcut("within(com.chohee.model.Triangle)")
+    public void allTriangleMethods() {}
+
 }
